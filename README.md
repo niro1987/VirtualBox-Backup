175837d489ae03f6a37e397163e7f280f456b287
Niels <niro1987@users.noreply.github.com>
2020-06-08 10:41:34 +0200

    Init - Create Git

---
+++ README.md
@@ -0,0 +1,79 @@
+# VirtualBox Backup
+An automated backup for Oracle VirtualBox VMs in Windows
+
+- [VirtualBox Backup](#virtualbox-backup)
+- [Installation](#installation)
+- [Usage](#usage)
+  - [Backup Folder](#backup-folder)
+  - [Shutdown Mode](#shutdown-mode)
+  - [Compression Mode](#compression-mode)
+  - [Cleanup Mode](#cleanup-mode)
+  - [Name Prefix](#name-prefix)
+
+# Installation
+1. Clone or copy this repository to the desired backup folder.
+2. Create a basic task to periodically start **VirtualBox Backup.bat** with [Task Scheduler](https://www.google.com/search?q=Windows+Task+Scheduler&oq=Windows+Task+Scheduler).
+3. *Optional:* Apply below parameters depending on your requirements.
+
+# Usage
+All paramters are optional. If you do not pass a parameter, it will revert to it's default behavior as documented below.
+
+## Backup Folder
+```
+[ -b | --backupdir ] { PATH }
+```
+By default the VM Backup's are saved to the folder where you saved the repository (the same folder as **VirtualBox Backup.bat** to be exact). To change the default, pass the above parameter along with a valid path. A subfolder is created if it does not already exist with the same name as the VM.
+
+| Parameter | Description |
+| --------- | ----------- |
+| `--backupdir` | *(default)* Parent folder |
+| `--backupdir "%USERPROFILE%"` | C:\Users\UserName |
+| `--backupdir "C:\Backup"` | C:\Backup |
+
+## Shutdown Mode
+```
+[ -s | --shutdown ]  [ acpipowerbutton | savestate ]
+```
+The VM needs to be shut down to copy the required files. Pass the above parameter to change the shutdown mode. The VM is restarted when the backup has been made (if it was running).
+
+| Parameter | Description |
+| --------- | ----------- |
+| `--shutdown acpipowerbutton` | *(default)* Use The VM is shut down using the ACPI mode. This is the same as short pressing the powerbutton on your PC, shuts it down clean. |
+| `--shutdown savestate` | The VM's state is saved and powered down. This does not actually shut down the VM, it's more like stopping time. Not all VM's (OS's) can handle the *gap* in time. | 
+
+## Compression Mode
+```
+[ -c | --compress ]  [ 0 - 9 ]
+```
+***!!!*** In order to enable data compression you need to install [7-Zip](https://www.7-zip.org/) and [7-Zip Extra](https://www.7-zip.org/).
+
+The VM Backup Files can be compressed to a single file to save some diskspace and/or to make the backups easier to handle.
+
+| Parameter | Description |
+| --------- | ----------- |
+| `--compress 0` | *(default)* No compression. While this does not actually compress the files, it does make them easier to handle because the backup is reduced to a single file. |
+| `--compress [1 - 9]` | Set compression level: 1 (fastest) ... 9 (ultra). |
+
+## Cleanup Mode
+```
+[ -k | --keep ] [ 0 - ~ ]
+```
+Delete old VM Backup files (or folders) and keeps the last `[x]` files. If no [Prefix](#name-prefix) is set, all files and folders in the VM's backup subfolder are validated and possibly removed.
+
+| Parameter | Description |
+| --------- | ----------- |
+| `--keep 0` | *(default)* No cleanup. Keep all files. |
+| `--keep [x]` | Keep the `[x]` last created files/folder. |
+
+## Name Prefix
+```
+[ -p | --prefix ] { PREFIX }
+```
+Each backup is saved to a folder named after the VM. The backup file/folder is named after the date the backup was created on (exact string depends on regional settings). Pass the above parameter to prefix an additional string to the backup name, an additional space is automatically added to the `PREFIX` string.
+
+Use this paramter to create a [Grandfather-father-son](https://en.wikipedia.org/wiki/Backup_rotation_scheme)- like backup rotation. [Cleanup Mode](#cleanup-mode) maintains the same prefix and does not delete backup's with a different (or no) prefix if one is set.
+
+| Parameter | Description |
+| --------- | ----------- |
+| `--prefix ""` | *(default)* No prefix. |
+| `--prefix "Daily"` | Prefix the backup name with `"Daily"` |
---
+++ VirtualBox Backup.bat
@@ -0,0 +1,259 @@
+:: 
+:: Creates a backup of all VMs in Oracle VirtualBox
+:: 
+
+@ECHO OFF
+CLS
+
+:Initialize
+
+	SET "_README="
+	SET "_BACKUPDIR="
+	SET "_SHUTDOWN="
+	SET "_COMPRESS="
+	SET "_KEEP="
+	SET "_PREFIX="
+
+	:ParseParameters
+	CALL :getParamFlag "/?" "_README" "%~1" && SHIFT /1 && GOTO :ParseParameters
+	IF DEFINED _README (
+		ECHO:
+		ECHO:
+		ECHO VirtualBox Backup - Help
+		ECHO:
+		ECHO [ -b ^| --backupdir ] { PATH }                        - Set to change Backup Folder. Default: .\ (Same folder as this file)
+		ECHO [ -s ^| --shutdown ]  [ acpipowerbutton ^| savestate ] - Set to change Shutdown Mode. Default: acpipowerbutton (Clean shutdown)
+		ECHO [ -c ^| --compress ]  [ 0 - 9 ]                       - Set to change Compression Mode. Default: 0 (No compression)
+		ECHO [ -k ^| --keep ] [ 0 - ~ ]                            - Set to change Cleanup Mode. Default: 0 (All)
+		ECHO [ -p ^| --prefix ] { PREFIX }                         - Set to change Name Prefix. Default: "" (No prefix)
+		ECHO: 
+		ECHO Please read the full documentation on https://github.com/niro1987/VirtualBox-Backup#usage
+		ECHO:
+		EXIT /B 0 
+	)
+
+	CALL :getParameter "-b" "_BACKUPDIR" "%~1" "%~2" && SHIFT /1 && SHIFT /1 && GOTO :ParseParameters
+	CALL :getParameter "--backupdir" "_BACKUPDIR" "%~1" "%~2" && SHIFT /1 && SHIFT /1 && GOTO :ParseParameters
+
+	CALL :getParameter "-s" "_SHUTDOWN" "%~1" "%~2" && SHIFT /1 && SHIFT /1 && GOTO :ParseParameters
+	CALL :getParameter "--shutdown" "_SHUTDOWN" "%~1" "%~2" && SHIFT /1 && SHIFT /1 && GOTO :ParseParameters
+
+	CALL :getParameter "-c" "_COMPRESS" "%~1" "%~2" && SHIFT /1 && SHIFT /1 && GOTO :ParseParameters
+	CALL :getParameter "--compress" "_COMPRESS" "%~1" "%~2" && SHIFT /1 && SHIFT /1 && GOTO :ParseParameters
+
+	CALL :getParameter "-k" "_KEEP" "%~1" "%~2" && SHIFT /1 && SHIFT /1 && GOTO :ParseParameters
+	CALL :getParameter "--keep" "_KEEP" "%~1" "%~2" && SHIFT /1 && SHIFT /1 && GOTO :ParseParameters
+
+	CALL :getParameter "-p" "_PREFIX" "%~1" "%~2" && SHIFT /1 && SHIFT /1 && GOTO :ParseParameters
+	CALL :getParameter "--prefix" "_PREFIX" "%~1" "%~2" && SHIFT /1 && SHIFT /1 && GOTO :ParseParameters
+
+	IF NOT DEFINED _BACKUPDIR (
+		FOR %%i IN ("%~dp0.") DO SET "_BACKUPDIR=%%~fi"
+	)
+	IF NOT DEFINED _SHUTDOWN (
+		SET _SHUTDOWN=acpipowerbutton
+	)
+	IF NOT DEFINED _COMPRESS (
+		SET _COMPRESS=0
+	)
+	IF NOT DEFINED _KEEP (
+		SET _KEEP=0
+	)
+	IF NOT DEFINED _PREFIX (
+		SET "_PREFIX="
+	)
+
+	SET "_VBOXMANAGE=C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
+	SET "_7za=C:\Program Files\7-Zip\7za.exe"
+	SET "_DATE=%DATE%"
+	SET _ERROR=0
+	SET _WAITTIME=5
+
+	ECHO Starting VirtualBox Backup...
+	"%_VBOXMANAGE%" list vms
+	ECHO: 
+	ECHO Parameters...
+	ECHO Backup Folder: "%_BACKUPDIR%"
+	ECHO Shutdown Mode: "%_SHUTDOWN%"
+	IF EXIST "%_7za%" (
+		ECHO Compression Mode: %_COMPRESS%
+	) ELSE (
+		ECHO Compression Mode: Could not find "%_7za%"
+	)
+	ECHO Cleanup Mode: %_KEEP%
+	ECHO Name Prefix: "%_PREFIX%"
+	ECHO:
+	ECHO:
+
+	IF DEFINED _PREFIX (
+		SET "_PREFIX=%_PREFIX% "
+	)
+
+	FOR /F "tokens=* delims=" %%V IN ('"%_VBOXMANAGE%" list vms') DO CALL :VM_Initialize %%V
+	GOTO :Terminate
+
+	:VM_Initialize
+	:: Set Variables
+		SET "_VMNAME=%~1"
+		SET "_VMUUID=%~2"
+		SET "_VMINITSTATE="
+		SET _REQUESTOFF=0
+		SET _LOOPCOUNT=12
+
+		ECHO "%_VMNAME%"
+
+		:VM_PowerOff
+		:: Shut down the VM (if it's running)
+			CALL :GetState
+			IF /I "%_VMSTATE%"=="running" (
+				IF %_REQUESTOFF% EQU 0 (
+					ECHO VM is "%_VMSTATE%"..
+				)
+				GOTO :PowerOff
+			) ELSE (
+				ECHO VM is "%_VMSTATE%"..
+			)
+
+		:VM_CopyFiles
+		:: Copy the VM files
+			CALL :GetPath
+			ECHO Copy Files...
+			IF EXIST "%_7za%" (
+				ROBOCOPY "%_VMPATH%." "%TEMP%\%_VMUUID%" /E
+			) ELSE (
+				ROBOCOPY "%_VMPATH%." "%_BACKUPDIR%\%_VMNAME%\%_PREFIX%%_DATE%" /E
+			)
+
+		:VM_Start
+		:: Start the VM (if it was running)
+			IF /I "%_VMINITSTATE%"=="running" (
+				ECHO Starting VM...
+				"%_VBOXMANAGE%" startvm %_VMUUID% --type headless
+			)
+
+		:VM_Compress
+		:: Compress the VM Backup Files to a single compressed file
+			IF EXIST "%_7za%" (
+				ECHO Compress Files...
+				"%_7za%" a -mx%_COMPRESS% -sdel "%_BACKUPDIR%\%_VMNAME%\%_PREFIX%%_DATE%.7z" "%TEMP%\%_VMUUID%\*"
+				RD /S /Q "%TEMP%\%_VMUUID%"
+			)
+
+		:VM_Cleanup
+		:: Delete old backups
+		IF %_KEEP% GTR 0 (
+			ECHO Cleanup of old backups...
+			FOR /F "skip=%_KEEP% eol=: delims=" %%F IN ('DIR /T:C /B /O:-D "%_BACKUPDIR%\%_VMNAME%\%_PREFIX%*"') DO (
+				ECHO Delete "%_VMNAME%\%%~F"...
+				FOR %%Z IN ("%_BACKUPDIR%\%_VMNAME%\%%~F") DO (
+					IF "%%~aZ" GEQ "d" (
+						RD /S /Q "%_BACKUPDIR%\%_VMNAME%\%%~F"
+					) ELSE (
+						IF "%%~aZ" GEQ "-" (
+							DEL "%_BACKUPDIR%\%_VMNAME%\%%~F"
+						)
+					)
+				)
+			)
+		)
+
+	:VM_Terminate
+	:: That's all folks!
+		ECHO:
+		EXIT /B 0
+		GOTO :EOF
+
+:Terminate
+:: Check for errors and exit..
+	IF %_ERROR% EQU 100 (
+		SET _ERRORMSG="Timeout during PowerOff"
+	)
+	IF %_ERROR% GTR 0 (
+		NOW %_ERROR%: %_ERRORMSG%
+		NOW %_ERROR%: %_ERRORMSG% >> %_BACKUPDIR%\errorlog.txt
+	)
+	EXIT /B
+	GOTO :EOF
+
+:PowerOn
+:: Takes the VM UUID from %_VMUUID% and start the VM in headless mode
+	"%_VBOXMANAGE%" startvm %_VMUUID% --type headless
+	EXIT /B 0
+	GOTO :EOF
+
+:PowerOff
+:: Takes the VM UUID from %_VMUUID% and tries to shut it down
+	IF %_LOOPCOUNT% GTR 0 (
+		IF %_REQUESTOFF% EQU 0 (
+			SET _REQUESTOFF=1
+			ECHO VM Shutdown...
+			"%_VBOXMANAGE%" controlvm %_VMUUID% %_SHUTDOWN%
+		)
+		ECHO Waiting for VM to shut down...
+		TIMEOUT /t %_WAITTIME% /nobreak > nul
+		SET /A _LOOPCOUNT-=1
+		GOTO :VM_PowerOff
+	) ELSE (
+		SET _ERROR=100
+		GOTO :Terminate
+	)
+	GOTO :EOF
+
+:GetState
+:: Takes the VM UUID from %_VMUUID% and returns the state in %_VMSTATE%
+:: The %_VMSTATE% variable changes during shutdown, %_VMINITSTATE% does not
+	"%_VBOXMANAGE%" showvminfo %_VMUUID% --machinereadable > %TEMP%\%_VMUUID%.txt
+	FOR /F "tokens=2 delims==" %%A IN ('FINDSTR /I /B "VMState=" "%TEMP%\%_VMUUID%.txt"') DO (
+		SET "_VMSTATE=%%~A"
+	)
+	IF "%_VMINITSTATE%"=="" (
+		SET "_VMINITSTATE=%_VMSTATE%"
+	)
+	IF EXIST "%TEMP%\%_VMUUID%.txt" (
+		DEL "%TEMP%\%_VMUUID%.txt"
+	)
+	EXIT /B 0
+	GOTO :EOF
+
+:GetPath
+:: Takes the VM UUID from %_VMUUID% and returns the folder path in %_VMPATH%
+	"%_VBOXMANAGE%" showvminfo %_VMUUID% --machinereadable > %TEMP%\%_VMUUID%.txt
+	FOR /F "tokens=* delims=" %%A IN ('FINDSTR /I /B "CfgFile=" "%TEMP%\%_VMUUID%.txt"') DO (
+		FOR %%B IN (%%~A) DO (
+			SET "_VMPATH=%%~dpB"
+		)
+	)
+	IF EXIST "%TEMP%\%_VMUUID%.txt" (
+		DEL "%TEMP%\%_VMUUID%.txt"
+	)
+	EXIT /B 0
+	GOTO :EOF
+
+:getParameter
+:: Thanks to https://stackoverflow.com/a/47169024
+:: 1 CLI Parameter Name
+:: 2 Parameter Name
+:: 3 Active Parameter Name
+:: 4 Active Parameter Value
+	IF "%~3"=="%~1" (
+		IF "%~4"=="" (
+			SET "%~2="
+			EXIT /B 1
+		)
+		SET "%~2=%~4"
+		EXIT /B 0
+	)
+	EXIT /B 1
+	GOTO :EOF
+
+:getParamFlag
+:: Thanks to https://stackoverflow.com/a/47169024
+:: 1 CLI Parameter Name
+:: 2 Parameter Name
+:: 3 Active Parameter Name
+	IF "%~3"=="%~1" (
+		SET "%~2=TRUE"
+	EXIT /B 0
+	)
+	EXIT /B 1
+	GOTO :EOF
