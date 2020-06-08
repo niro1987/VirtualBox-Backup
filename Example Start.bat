44d626a547e5f00843028549f9d36d2adf4b59e9
Niels <niro1987@users.noreply.github.com>
2020-06-08 17:08:33 +0200

    updated README

--- Example Start.bat
+++ Example Start.bat
@@ -7,5 +7,5 @@ FOR %%C IN ("%~dp0.") DO SET "_VBBACKUP=%%~fC\VirtualBox Backup.bat"
 :: "[ -k | --keep ]      [ 0 - ~ ]                       - Set to change Cleanup Mode. Default: 0 (All)
 :: "[ -p | --prefix ]    { PREFIX }                      - Set to change Name Prefix. Default: "" (No prefix)
 
-:: Example - Modify to accordingly
+:: Example - Modify according to your needs
 "%_VBBACKUP%" --backupdir="" --shutdown=acpipowerbutton --compress=0 --keep=0 --prefix=""
